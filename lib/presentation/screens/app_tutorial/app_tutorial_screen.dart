01c71f87463f8ef9398165d28f0a17d077dbd6cf
Lizzully <sandralizzully@hotmail.com>
2023-09-09 10:48:53 -0600

    screens creada y barril actualizado

---
+++ lib/presentation/screens/app_tutorial/app_tutorial_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScreen extends StatelessWidget {
+    static const name = 'tutorial_screen';
+   
+  const AppTutorialScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+         child: Text('AppTutorialScreen'),
+      ),
+    );
+  }
+}
